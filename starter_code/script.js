2bdd9f03a263bcbcb11fcbaea4d76a1b7631eac8
Tom Bijesse <tom@bijesse.com>
2016-12-07 10:33:09 -0500

    task 4 direction

--- starter_code/script.js
+++ starter_code/script.js
@@ -8,6 +8,6 @@
 $( document ).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
     
-//Task 4: (project) Create a site that can display all songs from an artist specified by a user search. (This will require an input field for the user to type into)
+//Task 4: (project) Create a site that can display all songs from an artist specified by a user search. (This will require an input field for the user to type into.. OR a drop down menu populated with Artist names)
 
 });
