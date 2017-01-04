5f69ef34bd399aa6e98c5cce1796032d16ae0305
b3nn <benn@verybadposture.com>
2017-01-04 21:11:11 +0000

    More notes

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,43 +1,32 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
-//console.log(library.music.Michael_Jackson[0].year);
-console.log(library.music[0].albums[0].year);
 
 //Task 2: Console the name of the song Lady Gaga released in 2006 
-//console.log(library.music.two_thousands["Lady_Gaga"][0].song);
-console.log(library.music[5].albums[0].song);
+
 
 $( document ).ready(function() {
     //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-    $("#song").html(library.music[6].albums[0].song);
+
     
     //Task 4: (project) Create a site that can display all songs from an artist specified by a user search. (This will require an input field for the user to type into.. OR a drop down menu populated with Artist names)
-    /*var years = Object.keys(library.music);
-    for( var x =0; x< years.length; x++){
-        var artists = Object.keys(library.music[years[x]]);
+    
+        //Step 4.1 - Loop through all the music objects and console.log the artists
         
-        for (var y=0; y< artists.length; y++){
-            var value = artists[y];
+        //Step 4.2 - fill in the "#artists" select box with each artist name as an option
+        //      Examples: https://gist.github.com/paulund/5861214
+        
+        //Step 4.3 - Fill in the ID of the form field to handle the submission 
+        $( "#songsearch" ).submit(function( event ) {
+          event.preventDefault();
+          
+          // 4.3 - Log the selected artist name
+  
+          // 4.4 - Loop over all the artists in music object until you find a match
             
-            $("#artists").append($('<option/>', { 
-                value: value,
-                text : value 
-            }));
-        }
-        
-    }*/
-    
-    var my_artists = library.music;
-    for (var x = 0; x < my_artists.length; x++){
-        console.log(my_artists[x]);
-    }
-    
-    // Search
-    $( "#songsearch" ).submit(function( event ) {
-      event.preventDefault();
-      var artists = $("#artists").val();
-      console.log(artists);
-      
-      
-    });
-    
+          // 4.5 - Log each of the songs from the artist to the console
+          
+          // 4.6 - Now, update the "songs" div with each of the song names
+          
+        });        
 });
+
+
